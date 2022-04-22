607bac7f6fbfbf8b7b8b11443e7bae912932ab2e
jocaisip <jocaisip10@gmail.com>
2022-04-22 01:41:06 -0400

    add latest files, tested app

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import pafy
 #helper packages
 import helper_predict
 import helper_process_and_predict as p
-import helper_generate_report as r
+import helper_generate_report_v2 as r
 
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
@@ -42,7 +42,17 @@ def home():
 @app.route('/logo_detection.html', methods=['GET', 'POST'])
 def logo_detection():
     
-    return render_template("logo_detection.html")
+    global logo_row
+    global logo_col
+    
+    import pandas as pd
+    
+    trained_logo = ["Laoshan Beer", "Heineken", "Healtheries", "Bobs Red Mill", "Baskin-Robbins", "Bellamys Australia", "Yellow Tail", "BreadTalk", "Columbus", "Gudasao", "Charbonnel et Walker", "De Cecco", "Colavita", "violet crumble", "Daves Gourmet", "Huy Fong Foods", "penfolds", "Chubang", "Dr. Oetker", "Haizhilan", "tianzhilan", "Yuanshi","Master Kong", "Weidendorf", "Yanjing Beer", "Snow Beer","Meadow fresh", "Blue Bell Ice Cream", "Weidendorf", "Cholula Hot Sauce", "Taitaile", "ChangJi", "Biggby Coffee","Weidamei", "Bacardi", "Hortex", "Liubiju","BelGioioso", "evian", "Laohenghe", "Bold Rock Hard Cider","Mengzhilan", "Driscolls"]
+    trained_logo_df = pd.DataFrame(trained_logo)
+    trained_logo_df = trained_logo_df.rename(columns={ 0 : 'Brand Logo'})
+    logo_row, logo_col = (trained_logo_df.to_dict('records'), trained_logo_df.columns.values)
+    
+    return render_template("logo_detection.html", logo_row=logo_row, logo_col=logo_col)
 
 
 @app.route('/upload', methods=['GET', 'POST'])
@@ -50,44 +60,67 @@ def file_upload():
     
     global filename
     global input_filename
-
+    global file_type
     
+    global image_filename
+    global video_filename
+
     #get uploaded file
-    if request.method == "POST":
+    if request.method == "POST":    
         f = request.files['logofile']
-        
-        #check for correct file type
+            #check for correct file type
+        #try:
         if f.filename.endswith(('.png', '.jpg', '.jpeg', '.mp4')):
             # if file type is correct, save to upload_path
             f.save(os.path.join(upload_path, secure_filename(f.filename)))
+            
+            # if f.filename.endswith(('.png', '.jpg', '.jpeg')):
+            #     image_filename = secure_filename(f.filename)
+            #     video_filename = False
+            #     filename = image_filename
+            # else:
+            #     video_filename = secure_filename(f.filename)
+            #     image_filename = False
+                
+            file_type = f.filename.split(".")
+            file_type = file_type[1]
             
             # get output filename
             detected_file = secure_filename(f.filename)
             filename = detected_file
             input_filename = filename
             output_filename = filename
-        
-    return render_template("logo_detection.html", input_filename=input_filename)
+                
+    return render_template("logo_detection.html", logo_row=logo_row, logo_col=logo_col, file_type=file_type, input_filename=input_filename)
 
+        #except Exception as e:
+         #   return str("Incorrect file type, please upload a PNG, JPG, JPEG, or MP4 file.")
+        
+        
 
 #Display input file    
 @app.route('/display/<filename>')
 def display_video(filename):
 	#print('display_video filename: ' + filename)
-	return redirect(url_for('static', filename='uploads/' + input_filename))
+    
+    return redirect(url_for('static', filename='uploads/' + input_filename))
+
 
 
 @app.route('/detect', methods=['GET', 'POST'])
 def detect():
     
-    # pass file to model for prediction
-    p.process_and_predict(os.path.join(upload_path, filename))
-    r.generate_report(labelmapfile='model/data/logo50.yaml', labelpath=detections_path, labelfile='label.txt')
-    
-    output_filename = input_filename
-    done_msg = "Detection done! Thank you for using LogoSpace!"
+    try:
+        # pass file to model for prediction
+        p.process_and_predict(os.path.join(upload_path, filename))
+        records, colnames = r.generate_report(labelmapfile='model/data/logo50.yaml', labelpath=detections_path, labelfile='label.txt')
+        
+        output_filename = input_filename
+        done_msg = "Detection done! Thank you for using LogoSpace!"
+    except Exception as e:
+        return str("Sorry, LogoSpace could not detect a logo, please upload a different file.")
 
-    return render_template("logo_detection.html", done_msg=done_msg, output_filename=output_filename)
+    return render_template("logo_detection.html", done_msg=done_msg, file_type=file_type, output_filename=output_filename, records=records, colnames=colnames, logo_row=logo_row, logo_col=logo_col)
 
 
 #Display output file    
@@ -105,7 +138,7 @@ def download():
     try:
         return send_from_directory(directory=detections_path, path=filename)
     except Exception as e:
-        return str(e)
+        return str("Sorry, there's no output file to download.")
 
 @app.route('/download_label', methods=['GET', 'POST'])
 def download_label():
@@ -114,7 +147,7 @@ def download_label():
     try:
         return send_from_directory(detections_path,'label.txt', as_attachment=True)
     except Exception as e:
-        return str(e)
+        return str("Sorry, there's no output label to download.")
     
 
 @app.route('/download_report', methods=['GET', 'POST'])
@@ -124,7 +157,7 @@ def download_report():
     try:
         return send_from_directory(detections_path,'output.csv', as_attachment=True)
     except Exception as e:
-        return str(e)
+        return str("Sorry, there's no output table to download.")
    
 
 
@@ -135,15 +168,19 @@ def youtube_url():
     
     global video_stream
     
-    youtube_url = request.form.get('youtube_url')
-    
-    video = pafy.new(youtube_url)
-    video_stream = video.getbest(preftype="mp4")
-    
-    video_stream.download(filepath=youtube_videos_path)
-    post_download = "Done! Please download the video with the Download button"
-    
-    return render_template("logo_detection.html", post_download=post_download)
+    try:   
+   
+        youtube_url = request.form.get('youtube_url')
+        
+        video = pafy.new(youtube_url)
+        video_stream = video.getbest(preftype="mp4")
+        
+        video_stream.download(filepath=youtube_videos_path)
+        post_download = "Download Ready! Please click Download Youtube Video to download"
+        
+        return render_template("logo_detection.html", post_download=post_download, logo_row=logo_row, logo_col=logo_col)
+    except Exception as e:
+        return str("Sorry, that youtube video cannot be downloaded, please try another video.")
     
 @app.route("/download_youtube", methods=['GET', 'POST'])
 def download_youtube():
@@ -151,7 +188,7 @@ def download_youtube():
     try:
         return send_from_directory(youtube_videos_path, path=video_stream.filename, as_attachment=True)
     except Exception as e:
-        return str(e)
+        return str("Sorry, there's no youtube video to download. Please click 'Start Download' and wait for the success message and try again.")
 
 
 if __name__ == "__main__":
